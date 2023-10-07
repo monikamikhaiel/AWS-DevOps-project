0e69cfc48674de1a4c3f01df266b8bdff61f74cf
monika mikhaiel <vnpb4155@orange.com>
2023-10-07 19:39:44 +0300

    Add EKS

--- infrastructure-terraform/outputs.tf
+++ infrastructure-terraform/outputs.tf
@@ -1,12 +1,12 @@
 output "vpc_arn" {
   value=module.vpc.vpc_arn
 }
-# output "eks_arn" {
-#    value=module.eks.cluster_arn
-# }
+output "eks_arn" {
+   value=module.eks.cluster_arn
+}
 output "ecr_repository_url" {
    value=module.ecr.repository_url
 }
-output "all_vpc_module"{
-   value=module.vpc
-}
+# output "all_vpc_module"{
+#    value=module.vpc
+# }
