25084e483e503fb298a5956dac3a63ee0cfc900f
monika mikhaiel <vnpb4155@orange.com>
2023-10-07 16:24:51 +0300

    adding credentials as vars

--- infrastructure-terraform/main.tf
+++ infrastructure-terraform/main.tf
@@ -19,6 +19,9 @@ terraform {
 provider "aws" {
   profile = "default"
   region = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
+
 }
 #create vpc
 module "vpc" {
--- infrastructure-terraform/variables.tf
+++ infrastructure-terraform/variables.tf
@@ -2,3 +2,5 @@ variable "region" {}
 variable "eks_instance_type"{}
 variable "Environment"{}
 variable "Terraform"{}
+variable "access_key"{}
+variable "secret_key"{}
