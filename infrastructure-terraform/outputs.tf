85e7215c83a2d31eb6b54f8bf17475cf62a239e8
monika mikhaiel <vnpb4155@orange.com>
2023-10-07 19:28:35 +0300

    new credentials

--- infrastructure-terraform/outputs.tf
+++ infrastructure-terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "vpc_arn" {
   value=module.vpc.vpc_arn
 }
-output "eks_arn" {
-   value=module.eks.cluster_arn
-}
+# output "eks_arn" {
+#    value=module.eks.cluster_arn
+# }
 output "ecr_repository_url" {
    value=module.ecr.repository_url
 }
